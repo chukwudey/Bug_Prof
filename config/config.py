c0966a9b87323830a4c092ef4baff58da76550bd
chukwudey <chuks.julian.cj@gmail.com>
2024-09-06 21:03:16 +0100

    3rd Commit

---
+++ config/config.py
@@ -0,0 +1,11 @@
+class Config:
+    URL = "https://automationplayground.com/crm/login.html"
+    UserName = "chuks@gmail.com"
+    Password = "123456"
+
+
+class ConfigNewCustomer:
+    Email = "chuks@gmail.com"
+    Firstname = "Chuks"
+    Lastname = "julian"
+    City = "Lagos"
--- test_suite.py
+++ test_suite.py
@@ -6,6 +6,7 @@ from selenium import webdriver
 from LoginPage.LoginPage_test import LoginPage
 from NewCustomerPage.NewCustomerPage import NewCustomerPage
 from SignoutPage.SignoutPage import SignOutPage
+from config.config import Config, ConfigNewCustomer
 
 
 @pytest.fixture(scope="session")
@@ -21,7 +22,7 @@ def driver_setup():
 def login(driver_setup):
     driver = driver_setup
     login_page = LoginPage(driver)
-    login_page.login_url("https://automationplayground.com/crm/login.html")
+    login_page.login_url(Config.URL)
     return login_page
 
 
@@ -29,8 +30,8 @@ time.sleep(5)
 
 
 def test_login_page(login):
-    login.enter_email("chuks@gmail.com")
-    login.enter_password("123456")
+    login.enter_email(Config.UserName)
+    login.enter_password(Config.Password)
     login.enter_rememberme()
     login.enter_submit()
 
@@ -38,10 +39,10 @@ def test_login_page(login):
 def test_new_customer_page_automation_play_ground(login):
     test_new_customer = NewCustomerPage(login.driver)
     test_new_customer.new_customer()
-    test_new_customer.email("chuks@gmail.com")
-    test_new_customer.firstname("Chuks")
-    test_new_customer.lastname("julian")
-    test_new_customer.city("Lagos")
+    test_new_customer.email(ConfigNewCustomer.Email)
+    test_new_customer.firstname(ConfigNewCustomer.Firstname)
+    test_new_customer.lastname(ConfigNewCustomer.Lastname)
+    test_new_customer.city(ConfigNewCustomer.City)
     test_new_customer.state()
     test_new_customer.gender()
     test_new_customer.promotional_list()
